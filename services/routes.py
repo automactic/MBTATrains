2586e9d491ff375b719ba88cf7877861d25e2092
Chris Li <chris@kiwix.org>
2020-02-01 20:12:20 -0500

    route service

---
+++ services/routes.py
@@ -0,0 +1,32 @@
+import asyncpg
+
+from entities import Route
+
+
+class RouteService:
+    def __init__(self, conn: asyncpg.Connection):
+        self.conn = conn
+
+    async def list(self):
+        records = await self.conn.fetch('''
+            SELECT id, name, description, type, sort_order, color, text_color FROM routes
+        ''')
+        routes = [Route(**record) for record in records]
+        return routes
+
+    async def upsert(self, routes: [Route]):
+        values = [(
+            route.id,
+            route.name,
+            route.description,
+            route.type,
+            route.sort_order,
+            route.color,
+            route.text_color,
+        ) for route in routes]
+        await self.conn.executemany('''
+            INSERT INTO routes (id, name, description, type, sort_order, color, text_color)
+            VALUES ($1, $2, $3, $4, $5, $6, $7)
+            ON CONFLICT (id)
+            DO UPDATE SET name=$2, description=$3, type=$4, sort_order=$5, color=$6, text_color=$7;
+        ''', values)
--- sql.py
+++ sql.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Table, Column, Integer, String, MetaData
+from sqlalchemy import create_engine
+import asyncpg
 
 
 metadata = MetaData()
@@ -12,3 +14,21 @@ routes = Table(
     Column('color', String),
     Column('text_color', String),
 )
+
+
+def create_tables():
+    # create table if not exist
+    engine = create_engine('postgres://postgres@localhost/postgres')
+    conn = engine.connect()
+    conn = conn.execution_options(isolation_level="AUTOCOMMIT")
+    results = conn.execute("SELECT * FROM pg_database WHERE datname = 'mbta';").fetchone()
+    if not results:
+        conn.execute("CREATE DATABASE mbta;")
+
+    # create tables
+    engine = create_engine('postgres://postgres@localhost/mbta')
+    metadata.create_all(engine)
+
+
+async def create_asyncpg_conn() -> asyncpg.Connection:
+    return await asyncpg.connect('postgres://postgres@localhost/mbta')
